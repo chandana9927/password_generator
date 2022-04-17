ca6295283200c3e5d2776be8faa5543a6e13c767
Chandana Murthy <chandanamurthy9927@gmail.com>
2022-04-18 04:59:20 +0530

    Implements random password generator.

---
+++ password_generator.py
@@ -0,0 +1,23 @@
+from multiprocessing.sharedctypes import Value
+import string
+import random
+import sys
+
+SIZE = 10
+argv_len = len(sys.argv)
+if argv_len > 2:
+    raise ValueError("Please provide just the desired password length as argument.")
+
+if len(sys.argv) == 2:
+    try:
+        SIZE = int(sys.argv[1])  
+    except ValueError:
+        raise ValueError("Please enter a valid integer as argument.")
+    
+if SIZE < 1 or SIZE > 24:
+    raise ValueError("Password size must be between 1 and 24.")  
+
+def pw_gen(size=10, ch=string.ascii_letters + string.digits + string.punctuation):
+    return ''.join(random.choice(ch) for _ in range(size))
+
+print(pw_gen(size=SIZE))
